f9d11bf02602736c6f7ba552fff6d36623210f86
ananyasharma2171-debug <ananyasharma2171@gmail.com>
2026-03-20 14:40:22 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask_mysqldb import MySQL
+from flask_cors import CORS
+import os
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
+CORS(app)
+
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
 
 mysql = MySQL(app)
 
