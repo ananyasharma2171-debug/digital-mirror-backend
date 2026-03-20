c501f4091131645bb1b3016ca729b62badf46886
ananyasharma2171-debug <ananyasharma2171@gmail.com>
2026-03-20 16:15:50 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
-app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
-app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
-app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
+#database configuration
+app.config['MYSQL_HOST'] = os.environ.get('MYSQLHOST')
+app.config['MYSQL_USER'] = os.environ.get('MYSQLUSER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQLPASSWORD')
+app.config['MYSQL_DB'] = os.environ.get('MYSQLDB')
+app.config['MYSQL_PORT'] = int(os.environ.get('MYSQLPORT', 3306))
 
 mysql = MySQL(app)
 
@@ -31,7 +33,3 @@ app.register_blueprint(analytics)
 @app.route('/')
 def home():
     return "Backend Running 🚀"
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
