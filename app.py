353c062e84bce07142627f5425bf6ad94ba1af88
ananyasharma2171-debug <ananyasharma2171@gmail.com>
2026-04-07 15:09:02 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ from flask_mail import Mail, Message
 app = Flask(__name__)
 CORS(app)
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('ananyasharma2171@gmail.com')
-app.config['MAIL_PASSWORD'] = os.environ.get('ondogclyzyhgfdf')
+app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER')
+app.config['MAIL_PORT'] = int(os.environ.get('MAIL_PORT', 587))
+app.config['MAIL_USE_TLS'] = os.environ.get('MAIL_USE_TLS') == 'True'
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
 mail = Mail(app)
 
